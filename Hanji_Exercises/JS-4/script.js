d357cbe32e27b862c1bff66a394a716b7473a791
hanji-exe <h.esmaelfahad@gmail.com>
2025-02-28 02:14:26 +0800

    JS-4

---
+++ Hanji_Exercises/JS-4/script.js
@@ -0,0 +1,21 @@
+
+  
+
+  for (let j = 1; j <= 10; j++) {
+    console.log(`Number: ${j}`);
+  }
+
+  let hobbies = ["Travelling", "Photography", "Gardening", "Cooking", "Painting", "Dancing", "Singing", "Swimming", "Cycling", "Running"];
+  
+  //I try this to check the type if "Object" kaya Nevernmind
+  console.log(typeof hobbies);
+
+  let index = 0;
+  while (index < hobbies.length) {
+    // Log each hobby and see if it works in the console
+
+    console.log(`Hobby ${index+1}`, hobbies[index]);
+
+    // Increment the index for the next iteration
+    index++;
+  }

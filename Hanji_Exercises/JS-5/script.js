74e347432c942213a4ce18696594cda7386741d8
hanji-exe <h.esmaelfahad@gmail.com>
2025-02-28 23:50:00 +0800

    JS-5

---
+++ Hanji_Exercises/JS-5/script.js
@@ -0,0 +1,6 @@
+let favoriteMovies = [ "Inception", "The Matrix", "Interstellar", "The Dark Knight", "Pulp Fiction"];
+let i;
+
+for(i=0; i<favoriteMovies.length; i++) {
+    console.log(favoriteMovies[i]);
+}
